968a393989337940cbbae563d3c821a0bdff2d5d
Mirko <mirko.longo.1987@gmail.com>
2020-02-17 16:25:47 +0100

    1 step

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("Hello! I am an alert box!!");
+var nomeAnagrafico = prompt('Come ti chiami?');
+document.getElementById('nome').innerHTML = nomeAnagrafico;
+
+var cognomeAnagrafico = prompt('Il tuo cognome?');
+document.getElementById('cognome').innerHTML = cognomeAnagrafico;
+
+var colorePreferito = prompt('Il tuo colore prererito?');
+document.getElementById('colore').innerHTML = colorePreferito;
+
+var annoCorrente = annoCorrente.getFullYear();
+document.getElementById('data').innerHTML = annoCorrente;
